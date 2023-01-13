618d0825cfcc5ed4f04a2c4cd427652cdde585bf
Hosseinreza <hrakbari77@gmail.com>
2023-01-13 09:55:09 +0330

    Add comment for edit css

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ document.querySelector("h1")         ===  $("h1")
 document.querySelectorAll("button")  ===  $("button")
 
 */
+
+
+/*
+Edit css of an element  ( Warning : Editing css property from JS file is not a appropriate thing try to make css class and add it to the tag instead. )
+JavaScript   ::   document.getElementById("h1").style.color = "red";
+jQuery       ::   $("tag").css("propertName","value");
+*/
 $("h1").css("color","red"); 
+
+$("h1").addClass()
